978a91028aad0338373729ef53f0c729a065dffd
Preeti <preetinarang2006@gmail.com>
2023-06-24 13:32:12 +0530

    Check the updated files

---
+++ TestProject/src/TestModule1/Hello.java
@@ -0,0 +1,21 @@
+package TestModule1 ;
+
+public class Hello {
+	public static void main(String[] args) {
+		
+	/*	int a=1;
+		int b=2;
+		int sum=add(a,b);
+		System.out.println("The sum is "+sum);*/
+		
+		for(int i=0;i<10;i++) {
+			System.out.println("Hey "+i);
+		}
+		}
+	
+	private static int add(int one,int two) {
+		int sum=one+two;
+		sum=6;
+		return sum;
+	}
+}
