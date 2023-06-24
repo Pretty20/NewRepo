d7b2d4a814d4ff4c6788f07a902427ce08c15f35
Preeti <preetinarang2006@gmail.com>
2023-06-24 18:54:09 +0530

    hi

--- TestProject/src/TestModule1/Hello.java
+++ TestProject/src/TestModule1/Hello.java
@@ -3,11 +3,6 @@ package TestModule1 ;
 public class Hello {
 	public static void main(String[] args) {
 		
-	/*	int a=1;
-		int b=2;
-		int sum=add(a,b);
-		System.out.println("The sum is "+sum);*/
-		
 		for(int i=0;i<10;i++) {
 			System.out.println("Hey "+i);
 		}
